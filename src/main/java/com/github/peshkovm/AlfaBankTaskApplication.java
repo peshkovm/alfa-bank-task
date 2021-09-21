202646d427f2eae39a300330b79a1c3143d26463
Maksim Peshkov <peshmaxan@gmail.com>
2021-09-21 14:22:01 +0300

    Initial commit

---
+++ src/main/java/com/github/peshkovm/AlfaBankTaskApplication.java
@@ -0,0 +1,13 @@
+package com.github.peshkovm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfaBankTaskApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AlfaBankTaskApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/github/peshkovm/AlfaBankTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.peshkovm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfaBankTaskApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
